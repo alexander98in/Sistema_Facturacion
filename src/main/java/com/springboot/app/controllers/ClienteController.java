a4bf067316c5d1f699fa42a9e23a474de6451f34
alexander98in <alexander98in@gmail.com>
2023-05-24 12:27:55 -0500

    Implementando Paginator

--- src/main/java/com/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/springboot/app/controllers/ClienteController.java
@@ -3,6 +3,9 @@ package com.springboot.app.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,12 +13,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springboot.app.models.entity.Cliente;
 import com.springboot.app.models.service.IClienteService;
+import com.springboot.app.util.paginator.PageRender;
 
 import jakarta.validation.Valid;
 
@@ -27,9 +32,16 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name="page", defaultValue = "0") int page, Model model) {
+		
+		Pageable pageRequest =  PageRequest.of(page, 4);
+		
+		Page<Cliente> clientes = clienteService.findAll(pageRequest);
+		
+		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
 		return "listar";
 	}
 	
