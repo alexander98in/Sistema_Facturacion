de6da1a6c6574f5d1f45cb9dd17946971d586cdd
alexander98in <alexander98in@gmail.com>
2023-05-31 00:04:40 -0500

    Asociaciones: OneToMany unidireccional - Clases Entity Factura y ItemFactura

---
+++ src/main/java/com/springboot/app/models/entity/ItemFactura.java
@@ -0,0 +1,45 @@
+package com.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="facturas_items")
+public class ItemFactura implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer cantidad;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public Long calcularImporte() {
+		return this.cantidad.longValue();
+	}
+}
